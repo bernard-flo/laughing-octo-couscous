320e1977e2167233339cdebdf42ce6fecf663296
Dongwoon Son <dongwoon.son@dreamus.io>
2024-11-04 21:29:23 +0900

    Refactor login component of presenter page

--- fe/src/jsMain/kotlin/fe/page/presenter/PresenterPage.kt
+++ fe/src/jsMain/kotlin/fe/page/presenter/PresenterPage.kt
@@ -3,21 +3,14 @@ package fe.page.presenter
 import fe.client.presenter.PresenterClient
 import mui.icons.material.StarOutline
 import mui.material.Box
-import mui.material.Button
-import mui.material.ButtonVariant
 import mui.material.Card
-import mui.material.FormControlVariant
 import mui.material.PaperVariant
-import mui.material.TextField
 import mui.material.Typography
 import mui.material.styles.TypographyVariant
 import mui.system.sx
 import react.FC
 import react.Props
-import react.ReactNode
-import react.StateSetter
 import react.dom.html.ReactHTML.div
-import react.dom.onChange
 import react.useRef
 import react.useState
 import shared.domain.common.ChatItem
@@ -26,12 +19,10 @@ import web.cssom.BoxSizing
 import web.cssom.Color
 import web.cssom.Display
 import web.cssom.FlexDirection
-import web.cssom.JustifyContent
 import web.cssom.Overflow
 import web.cssom.pct
 import web.cssom.px
 import web.cssom.vh
-import web.html.InputType
 
 internal val PresenterPage = FC<Props> {
 
@@ -162,50 +153,6 @@ private val PresenterGameComponent = FC<PresenterGameComponentProps> { props ->
                     }
                 }
             }
-        }
-    }
-}
-
-
-private external interface PresenterLoginComponentProps : Props {
-    var presenterClient: PresenterClient
-    var enterFailed: Boolean
-    var setEnterFailed: StateSetter<Boolean>
-}
-
-private val PresenterLoginComponent = FC<PresenterLoginComponentProps> { props ->
-
-    var password by useState<String>("")
-
-    val doClientEnter = { props.presenterClient.enter(password) }
-
-    Box {
-        sx {
-            display = Display.grid
-            justifyContent = JustifyContent.center
-            marginTop = 100.px
-            gap = 40.px
-        }
-        Typography {
-            variant = TypographyVariant.h2
-            +"Presenter"
-        }
-        TextField {
-            type = InputType.password
-            variant = FormControlVariant.outlined
-            label = ReactNode("비밀번호")
-            error = props.enterFailed
-            helperText = if (props.enterFailed) ReactNode("비밀번호를 정확하게 입력해주세요") else null
-            onChange = {
-                password = it.target.asDynamic().value
-                props.setEnterFailed(false)
-            }
-            onKeyUp = { if (it.key == "Enter") doClientEnter() }
-        }
-        Button {
-            variant = ButtonVariant.contained
-            onClick = { doClientEnter() }
-            +"Login"
         }
     }
 }
